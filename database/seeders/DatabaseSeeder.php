c2e4de313e86aa8a9e05d4e3bfba63eac54a6442
Agustin Zamar <agustinzamar33@gmail.com>
2022-07-04 21:13:50 -0300

    add default users to local seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\Article;
 use App\Models\Link;
 use App\Models\Role;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,16 +16,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
 
-        if (env('APP_ENV') !== 'production') {
+        if (env('APP_ENV') === 'local') {
             Article::factory()->count(10)->create();
             Link::factory()->count(15)->create();
+
+            \App\Models\User::factory()->create([
+                'name' => 'Test User',
+                'email' => 'user@example.com',
+                'role' => 2
+            ]);
+
+            \App\Models\User::factory()->create([
+                'name' => 'Test Admin',
+                'email' => 'admin@example.com',
+                'role' => 1
+            ]);
         }
 
         Role::factory()->create(['name' => 'admin']);
